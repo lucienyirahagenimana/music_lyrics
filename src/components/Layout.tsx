53420f1e9b2af1b69a31b35d851f017c0c7ac88c
lucienyirahagenimana <106026908+lucienyirahagenimana@users.noreply.github.com>
2023-07-22 19:38:26 +0200

    music lyrics finder using some API

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from "react";
+import Header from "./Header";
+import Meta from "./Meta";
+import { Toaster } from "react-hot-toast";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Meta />
+      <main className="min-w-screen relative flex min-h-screen flex-col items-center gap-6 bg-zinc-900 p-4 font-sans">
+        <Header />
+        {children}
+      </main>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: "#27272a",
+            color: "#fce7f3",
+          },
+          position: "top-right",
+          duration: 1500,
+        }}
+      />
+    </>
+  );
+}
